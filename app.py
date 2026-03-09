a8174d92b5fc8a51471427bc9f52f999f40d9441
AlfaPacktor <packtor@mail.ru>
2026-03-09 18:06:15 +0300

    Update app.py

--- app.py
+++ app.py
@@ -137,7 +137,7 @@ for i,(q,answer) in enumerate(questions):
 
 # ------------------- Финальный блок -------------------
 if st.session_state.step == 7:
-    st.markdown("<h2 style='text-align:center'>Ты прошла все испытания!</h2>", unsafe_allow_html=True)
+    st.markdown("<h2 style='text-align:center'>Ты прошла все  испытания!</h2>", unsafe_allow_html=True)
 
     if not st.session_state.gift_opened:
 
@@ -145,7 +145,7 @@ if st.session_state.step == 7:
 
         # Важно: все строки внутри with col2 должны иметь одинаковый отступ (4 пробела)
         with col2:
-            if st.button("🎁🎁🎁 Открыть и забрать подарок 🎁🎁🎁"):
+            if st.button("🎁🎁🎁  Открыть и забрать подарок 🎁🎁🎁"):
                 st.session_state.gift_opened = True
                 st.rerun()
 
